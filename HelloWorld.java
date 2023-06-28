017e8dcd5de37d883e71bed48bc3253af8f37634
alexpitronot <alexpitronot@gmail.com>
2023-06-28 17:06:55 +0300

    Урок 2

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name ="Alex";
+        System.out.println("Hello, World!");
+        System.out.println("My name is " + name);  
+    }
+}
