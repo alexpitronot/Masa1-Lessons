76755dc503c66fc7db0acfd12cd9fcc72185ab14
alexpitronot <alexpitronot@gmail.com>
2023-06-28 21:10:01 +0300

    Конец 2 урока

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,19 @@
 public class HelloWorld {
     public static void main(String[] args) {        
-        System.out.println("Hello, World!");
-        // String name ="Alex";
-        // System.out.println("My name is " + name);  
+        // System.out.println("Alex");
+        // System.out.println("Qiryat Yam");
+        // System.out.println("45");
+        // System.out.print("Q");
+        // System.out.print("A");
+        String $name ="Alex";
+        System.out.println("My name is " + $name);
+        
+        int age1 = 45;
+        System.out.println("I'm " + age1 + " old");
+    //    int x;
+    //    x=4;
+    //    System.out.println("x=" + x);
+    //    x=6;
+    //    System.out.println("x=" + x);
     }
 }
