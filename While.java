75a513e7f5405c94789b18f6e042912a57345a70
Alex Gorbachov <alexpitronot@gmail.com>
2023-07-03 15:48:11 +0300

    Урок 4

---
+++ While.java
@@ -0,0 +1,23 @@
+public class While {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i < 8) {
+           System.out.print(i + " ");
+           i++;
+        }
+
+        // int i = 1;
+        // while (i < 0) {
+        //     System.out.print(i + " ");
+        //     i++;
+        // }
+
+        // int i = 1;
+        // while (true) {
+        //     System.out.print(i + " ");
+        //     i++;
+        // }
+
+    }
+}
