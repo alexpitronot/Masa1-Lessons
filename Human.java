00230e066783eda0a60cac1fd02abe8309f10bfc
Alex Gorbachov <alexpitronot@gmail.com>
2023-08-10 16:41:25 +0300

    Урок 13

---
+++ Human.java
@@ -0,0 +1,11 @@
+// public abstract class Human {
+public class Human {
+    private String name;
+    public String getName () {
+        return name;
+    }
+    public Human(String name) {
+        this.name = name;
+    }
+}
+
--- Java/Class13.java
+++ Java/Class13.java
@@ -3,6 +3,8 @@ public class Class13 {
 public static void main(String[] args) {
     Dobermann d = new Dobermann();
     d.barking();
-}
 
+    Human alex = new Human ("Alex");   
+    System.out.println(alex.getName());
+}
 }
